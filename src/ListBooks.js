d947031152fa1ab04fdeadbc5282d061704a4bec
jacktulv <shkinglu@163.com>
2018-02-13 19:47:28 +0800

    修复bug

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -84,7 +84,7 @@ class ListBooks extends Component {
                               <div className="book-shelf-changer">
                                 <select value='read' onChange={(event)=>this.props.onchangeShelf(book,event.target.value)} >
                                   <option value="none" disabled>Move to...</option>
-                                  <option value="wantToRead">Currently Reading</option>
+                                  <option value="currentlyReading">Currently Reading</option>
                                   <option value="wantToRead">Want to Read</option>
                                   <option value="read">Read</option>
                                   <option value="none">None</option>
